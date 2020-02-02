2fa8e197e6fe1811e13e8f60c8180e3ae29879d3
Leo <vsleonid@gmail.com>
2020-02-02 08:40:45 +0100

    simple server

--- cmd/simple_server/main.go
+++ cmd/simple_server/main.go
@@ -2,13 +2,22 @@ package main
 
 import (
 	"github.com/godbus/dbus/v5"
+	"github.com/godbus/dbus/v5/introspect"
 	"github.com/google/uuid"
 	"log"
 )
 
+const intro = `
+<node>
+	<interface name="com.github.lvsl.Worker">
+		<method name="DoWork">
+			<arg direction="out" type="s"/>
+		</method>
+	</interface>` + introspect.IntrospectDataString + `</node>`
+
 type Worker struct{}
 
-func (w *Worker) DoWork() (string, *dbus.Error) {
+func (w Worker) DoWork() (string, *dbus.Error) {
 	token, err := uuid.NewRandom()
 	if err != nil {
 		return "", dbus.MakeFailedError(err)
@@ -30,6 +39,7 @@ func main() {
 
 	// Export object on the bus
 	conn.Export(w, "/", "com.github.lvsl.Worker")
+	conn.Export(introspect.Introspectable(intro), "/", "org.freedesktop.DBus.Introspectable")
 
 	// register on a bus
 	reply, err := conn.RequestName("com.github.lvsl.Worker", dbus.NameFlagDoNotQueue)
@@ -41,5 +51,7 @@ func main() {
 	}
 
 	// serve...
+    // dbus-send --session --print-reply --type=method_call
+    // --dest=com.github.lvsl.Worker / com.github.lvsl.Worker.DoWork
 	select {}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/lvsl/fosdem-2020-go-dbus-systemd
 
 go 1.13
 
-require github.com/godbus/dbus/v5 v5.0.3 // indirect
+require (
+	github.com/godbus/dbus/v5 v5.0.3 // indirect
+	github.com/google/uuid v1.1.1 // indirect
+)
